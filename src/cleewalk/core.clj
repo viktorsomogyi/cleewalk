fc38ee8dffb0d367ad82999340e72e9c23d976d7
Viktor Somogyi <viktorsomogyi@gmail.com>
2017-10-04 12:16:50 +0200

    Working tree walking that prints

--- src/cleewalk/core.clj
+++ src/cleewalk/core.clj
@@ -1,6 +1,31 @@
 (ns cleewalk.core)
+(ns cleewalk.core (:use clojure.pprint))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defrecord Node [left value right])
+
+(defn not-nil? [expr] (not (nil? expr)))
+
+(defn push-left
+  [stack]
+  (let [top (peek stack)]
+    (if (not-nil? top)
+      (recur (conj stack (:left top)))
+      (pop stack))))
+
+(defn has-next [stack] (not-nil? (peek stack)))
+
+(defn cleewalk_internal
+  "Walks the tree"
+  [stack]
+  (when (not (empty? stack))
+    (let [top (peek stack)
+          reduced (pop stack)]
+      (println (:value top))
+      (if (not-nil? (:right top))
+        (recur (push-left (conj reduced (:right top))))
+        (recur reduced)))))
+
+(defn cleewalk
+  "Walks the binary tree given in the first parameter"
+  [node]
+  (let [stack (list node)] (cleewalk_internal (push-left stack))))
--- test/cleewalk/core_test.clj
+++ test/cleewalk/core_test.clj
@@ -2,6 +2,54 @@
   (:require [clojure.test :refer :all]
             [cleewalk.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(ns cleewalk.core-test (:use clojure.pprint))
+
+(deftest testNotNil
+  (testing "Testing not-nil? true"
+    (let [
+      node (->Node nil 1 nil)
+      ](is (not-nil? node)))))
+
+(deftest testNil
+  (testing "Testing not-nil? false"
+    (let [
+      node (->Node nil 1 nil)
+      ](is (not (not-nil? (:left node)))))))
+
+(deftest testPeekStack
+  (testing "Testing peek"
+    (let [
+      stack '(1 2 3)
+      ](is (= 1 (peek stack)))
+      (is (= '(1 2 3) stack)))))
+
+(deftest testPopStack
+  (testing "Testing pop"
+    (let [
+      stack '(1 2 3)
+      ](is (= '(2 3) (pop stack))))))
+
+
+(defn recurring
+  "Recurring func"
+  [alist]
+  (let [l (peek alist)]
+    (pprint alist)
+    (when (not-nil? l)
+      (let [blist (pop alist)]
+        (recur (conj blist l))))))
+
+;(deftest testRecur
+;  (testing "Testing pop"
+;    (recurring '(1 2 3))))
+
+(deftest testClassicExample
+  (testing "Tests the classic interview example."
+    (let [
+      node4 (->Node nil 4 nil)
+      node2 (->Node node4 2 nil)
+      node6 (->Node nil 6 nil)
+      node5 (->Node nil 5 nil)
+      node3 (->Node node5 3 node6)
+      node1 (->Node node2 1 node3)
+      ](cleewalk node1))))
